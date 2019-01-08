6591ca165e9d61c853cfbac15bc194f56bfa32cf
huangsansui <674208615@qq.com>
2019-01-08 16:57:39 +0800

    :spark: Add util ListNode

---
+++ src/main/java/util/ListNode.java
@@ -0,0 +1,19 @@
+package util;
+
+/**
+ * Function:
+ *
+ * @author Huangqing
+ * @date 2019/1/8 16:56
+ * @since JKD 1.8
+ */
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
