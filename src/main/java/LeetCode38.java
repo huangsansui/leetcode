e3d64a6590e66f27d712a0176ae425fc67768e2d
huangsansui <674208615@qq.com>
2019-01-11 00:34:01 +0800

    :spark: Add leetcode 38

---
+++ src/main/java/LeetCode38.java
@@ -0,0 +1,32 @@
+/**
+ * Function:
+ *
+ * @author: Huangqing
+ * @Date: 2019/1/11
+ * @since: JDK 1.8
+ */
+public class LeetCode38 {
+
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        String str = countAndSay(n - 1) + "*";
+        int count = 1;
+        String s = "";
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                s += String.valueOf(count) + str.charAt(i);
+                count = 1;
+            }
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        LeetCode38 code = new LeetCode38();
+        System.out.println(code.countAndSay(3));
+    }
+}
