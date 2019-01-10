2d509721a0886fa31db7e70baa11f3e38e485408
huangsansui <674208615@qq.com>
2019-01-11 01:09:56 +0800

    :spark: Add leetcode 53

---
+++ src/main/java/LeetCode53.java
@@ -0,0 +1,26 @@
+/**
+ * Function:
+ *
+ * @author: Huangqing
+ * @Date: 2019/1/11
+ * @since: JDK 1.8
+ */
+public class LeetCode53 {
+
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = 0;
+        for (int i=0;i<nums.length;i++) {
+            sum += nums[i];
+            if (sum > max) {
+                max = sum;
+            }
+            // 如果sum < 0,不管nums[i + 1]是正数还是负数，
+            // 都有 sum + nums[i + 1] < nums[i + 1]
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return 0;
+    }
+}
