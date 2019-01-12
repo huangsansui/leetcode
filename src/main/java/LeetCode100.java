832fe76e36e13c6ccdaa453e995cc842ac4492a3
huangsansui <674208615@qq.com>
2019-01-12 15:06:30 +0800

    :spark: Add leetcode 100

---
+++ src/main/java/LeetCode100.java
@@ -0,0 +1,24 @@
+import util.TreeNode;
+
+/**
+ * Function:
+ *
+ * @author: Huangqing
+ * @Date: 2019/1/12
+ * @since: JDK 1.8
+ */
+public class LeetCode100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
---
+++ src/main/java/util/TreeNode.java
@@ -0,0 +1,15 @@
+package util;
+
+/**
+ * Function:
+ *
+ * @author: Huangqing
+ * @Date: 2019/1/12
+ * @since: JDK 1.8
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
