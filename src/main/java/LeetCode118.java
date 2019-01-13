456fe10257d6784e630a6c5d0c7c3f324feb32b8
huangsansui <674208615@qq.com>
2019-01-13 23:47:36 +0800

    :spark: Add leetcode 118

---
+++ src/main/java/LeetCode118.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Function:
+ *
+ * @author: Huangqing
+ * @Date: 2019/1/13
+ * @since: JDK 1.8
+ */
+public class LeetCode118 {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) {
+            return result;
+        }
+        int i = 1;
+        while (i <= numRows) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                if (i == 1) {
+                    list.add(1);
+                    break;
+                }
+                if (j == 0 || j == i - 1) {
+                    list.add(1);
+                } else {
+                    list.add(result.get(i - 2).get(j - 1) + result.get(i - 2).get(j));
+                }
+            }
+            result.add(list);
+            i++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LeetCode118 code = new LeetCode118();
+        List list = code.generate(5);
+    }
+}
