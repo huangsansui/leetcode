a2e43b2503d23c0b44b51f97577ca502ee7623bc
huangsansui <674208615@qq.com>
2022-04-19 14:54:13 +0800

    update LeetCode344.java

--- src/main/java/LeetCode344.java
+++ src/main/java/LeetCode344.java
@@ -17,6 +17,15 @@ public class LeetCode344 {
         }
     }
 
+    public void reverseString1(char[] s) {
+       int mid = s.length / 2;
+        for (int i = 0; i < mid; i++) {
+            char c = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = c;
+        }
+    }
+
     public static void main(String[] args) {
         LeetCode344 code = new LeetCode344();
         char[] s = new char[]{'h','e','l','l','o'};
