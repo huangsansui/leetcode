8264695bf5ab60023c1c6365240aaca99ac13f29
huangsansui <674208615@qq.com>
2022-04-20 21:36:37 +0800

    udpate LeetCode53.java

--- src/main/java/LeetCode53.java
+++ src/main/java/LeetCode53.java
@@ -23,4 +23,27 @@ public class LeetCode53 {
         }
         return max;
     }
+
+    /**
+     * 动态规划
+     * @param nums
+     * @return
+     */
+    public int maxSubArray1(int[] nums) {
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        LeetCode53 soultion = new LeetCode53();
+        soultion.maxSubArray1(new int[]{-2,1,-3,4,-1,2,1,-5,4});
+    }
+
 }
