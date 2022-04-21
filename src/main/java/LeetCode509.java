db078962d08a81b9fdfb0024c34e814c3687f58b
huangsansui <674208615@qq.com>
2022-04-21 17:32:12 +0800

    update LeetCode509.java

--- src/main/java/LeetCode509.java
+++ src/main/java/LeetCode509.java
@@ -18,6 +18,40 @@ public class LeetCode509 {
         return fib(N - 1) + fib(N - 2);
     }
 
+    public int fib1(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        return helper(dp, n);
+    }
+
+    private int helper(int[] dp, int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        dp[n] = helper(dp, n - 1) + helper(dp, n - 2);
+        return dp[n];
+    }
+
+    public int fib2(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int pre = 1, cur = 1;
+        for (int i = 3; i <= n; i++) {
+            int sum = pre + cur;
+            pre = cur;
+            cur = sum;
+        }
+        return cur;
+    }
+
     public static void main(String[] args) {
         LeetCode509 code = new LeetCode509();
         System.out.println(code.fib(4));
