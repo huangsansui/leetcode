62eec4196fd4fca1f01012760f12d8b88075db8f
huangsansui <674208615@qq.com>
2022-04-21 10:54:21 +0800

    add Offer4.java

--- src/main/java/LeetCode3.java
+++ src/main/java/LeetCode3.java
@@ -40,6 +40,20 @@ public class LeetCode3 {
         return res;
     }
 
+    public int lengthOfLongestSubstring1(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int j = 0;
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - j + 1);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         LeetCode3 code = new LeetCode3();
         System.out.println(code.lengthOfLongestSubstring("dvdf"));
