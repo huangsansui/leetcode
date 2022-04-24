11447636767a2fd9b2be41cbfba0c4ec4bd32311
huangsansui <674208615@qq.com>
2022-04-24 12:43:24 +0800

    add LeetCode118.java

--- src/main/java/LeetCode118.java
+++ src/main/java/LeetCode118.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Function:
@@ -35,7 +34,25 @@ public class LeetCode118 {
         return result;
     }
 
-    public static void main(String[] args) {
+    public List<List<Integer>> generate1(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        int i = 1;
+        while (i <= numRows) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                if (j == 0 || j == i - 1) {
+                    list.add(1);
+                } else {
+                    list.add(res.get(i - 2).get(j - 1) + res.get(i - 2).get(j));
+                }
+            }
+            res.add(list);
+            i++;
+        }
+        return res;
+    }
+
+        public static void main(String[] args) {
         LeetCode118 code = new LeetCode118();
         List list = code.generate(5);
     }
