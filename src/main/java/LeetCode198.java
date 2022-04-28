b8dda77536db7d5ab8dd295539cb7f79b5e4c929
huangsansui <674208615@qq.com>
2022-04-28 21:37:39 +0800

    update LeetCode198.java

--- src/main/java/LeetCode198.java
+++ src/main/java/LeetCode198.java
@@ -24,6 +24,45 @@ public class LeetCode198 {
         return result[result.length - 1];
     }
 
+    public int rob1(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        int i = 2;
+        while (i < nums.length) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+            i++;
+        }
+        return dp[nums.length - 1];
+    }
+
+    public int rob2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int pre = nums[0];
+        int cur = Math.max(nums[0], nums[1]);
+        int sum = cur;
+        int i = 2;
+        while (i < nums.length) {
+            sum = Math.max(nums[i] + pre, cur);
+            pre = cur;
+            cur = sum;
+            i++;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         LeetCode198 code = new LeetCode198();
         System.out.println(code.rob(new int[]{2, 1, 1, 9}));
